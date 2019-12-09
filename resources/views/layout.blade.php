8ae1224a13fefe33c86a9a9da85723c01f17f20e
Mister Nemo <miister.nemo@gmail.com>
2019-12-09 14:23:09 +0200

    Change views files structure

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+  </head>
+  <body class="text-center">
+
+    <div id="app"></div>
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        @include('header')
+        @yield('content')
+        @include('footer')
+    </div>
+
+    <script src="{{ mix('/js/app.js') }}"></script>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layout')
 
-    <title>{{ config('app.name') }}</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-  </head>
-  <body class="text-center">
-    
-    <div id="app"></div>
-
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <header class="masthead mb-auto">
-    <div class="inner">
-      <h3 class="masthead-brand">Cover</h3>
-      <nav class="nav nav-masthead justify-content-center">
-        <a class="nav-link active" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Contact</a>
-      </nav>
-    </div>
-  </header>
-
+@section('content')
   <main role="main" class="inner cover">
     <h1 class="cover-heading">Cover your page.</h1>
     <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
@@ -32,13 +8,4 @@
       <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
     </p>
   </main>
-
-  <footer class="mastfoot mt-auto">
-    <div class="inner">
-      <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-    </div>
-  </footer>
-</div>
-    <script src="{{ mix('/js/app.js') }}"></script>
-</body>
-</html>
+@stop
