2afa574c3fa7c6c1482483b860a2bd02a51edd40
Mister Nemo <miister.nemo@gmail.com>
2019-12-09 16:01:19 +0200

    Changing products list category sidebar

--- resources/views/product/products.blade.php
+++ resources/views/product/products.blade.php
@@ -3,18 +3,36 @@
 @section('content')
   <div class="row text-left">
 
-    <aside class="col-md-2 blog-sidebar">
+    <aside class="col-md-3 blog-sidebar">
 
       <div class="p-4">
-        <h4 class="font-italic">Categories</h4>
-        <ol class="list-unstyled mb-0">
-          <li><a href="#">Category name</a></li>
-          <li><a href="#">Category name</a></li>
-        </ol>
+		<div class="input-group mb-3">
+		  <input type="text" class="form-control" placeholder="Product name" aria-label="Product name" aria-describedby="button-addon2">
+		  <div class="input-group-append">
+		    <button class="btn btn-outline-secondary" type="button" id="button-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><path d="M21 21l-5.2-5.2"></path></svg></button>
+		  </div>
+		</div>
+        <h4 class="font-italic d-flex justify-content-between">Categories 
+        <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
+  			<button type="button" class="btn btn-secondary">Unselect</button>
+		</div>
+        </h4>
+		<div class="form-check">
+		  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+		  <label class="form-check-label" for="defaultCheck1">
+		    Category name
+		  </label>
+		</div>
+		<div class="form-check">
+		  <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+		  <label class="form-check-label" for="defaultCheck2">
+		    Category name
+		  </label>
+		</div>
       </div>
     </aside>
 
-	<div class="col-md-10 blog-main">
+	<div class="col-md-9 blog-main">
 
 	  <div class="row mb-2">
 	    <div class="col-md-6">
