6d679d1c9e1d29628dd3b0c63491bc667ef99c15
Mister Nemo <miister.nemo@gmail.com>
2019-12-10 11:16:07 +0200

    Add products table migration schema

---
+++ database/migrations/2019_12_10_083903_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('variation')->unsigned();
+            $table->string('name');
+            $table->longText('description');
+            $table->bigInteger('quantity')->unsigned();
+            $table->decimal('price');
+            $table->boolean('enabled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
