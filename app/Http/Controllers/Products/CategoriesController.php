6f4494ce44e16bf0c44d3ab2ac0d46b0dccecc99
Mister Nemo <miister.nemo@gmail.com>
2019-12-11 14:10:19 +0200

    Finished Categories and Products controllers for API get list

--- app/Http/Controllers/Products/CategoriesController.php
+++ app/Http/Controllers/Products/CategoriesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Products;
 
 use App\Categories;
+use App\ProductsCategories;
 use App\Http\Controllers\Controller;
 
 class CategoriesController extends Controller
 {
     private $Categories = null;
+    private $ProductsCategories = null;
     
     /**
      * Create a new controller instance.
@@ -17,6 +19,7 @@ class CategoriesController extends Controller
     public function __construct()
     {
         $this->Categories = new Categories();
+        $this->ProductsCategories = new ProductsCategories();
     }
 
     /**
@@ -26,6 +29,18 @@ class CategoriesController extends Controller
      */
     public function list()
     {
-        return $this->Categories->list();
+        $cat = collect(
+            $this->Categories->list()
+        )->keyBy('id');
+
+        $count = collect(
+            $this->ProductsCategories->count_and_group('product_id', 'category_id')
+        )->keyBy('category_id');
+
+        $cat_prod = collect(
+            $this->ProductsCategories->group_and_join('category_id', 'product_id')
+        )->keyBy('category_id');
+
+        return array_merge_by_int_key($cat, $count);
     }
 }
--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Products;
 
 use App\Products;
+use App\ProductsImages;
+use App\ProductsCategories;
 use App\Http\Controllers\Controller;
 
 class ProductsController extends Controller
 {
     private $Products = null;
+    private $ProductsImages = null;
+    private $ProductsCategories = null;
     
     /**
      * Create a new controller instance.
@@ -17,6 +21,8 @@ class ProductsController extends Controller
     public function __construct()
     {
         $this->Products = new Products();
+        $this->ProductsImages = new ProductsImages();
+        $this->ProductsCategories = new ProductsCategories();
     }
 
     /**
@@ -26,6 +32,22 @@ class ProductsController extends Controller
      */
     public function list()
     {
-        return $this->Products->list();
+        $prod = collect(
+            $this->Products->list()
+        )->keyBy('id');
+
+        $prod_varat = collect(
+            $this->Products->group_and_join('variation', 'id', 'variation_list')
+        )->keyBy('variation');
+
+        $prod_cat = collect(
+            $this->ProductsCategories->group_and_join('product_id', 'category_id', 'category_list')
+        )->keyBy('product_id');
+
+        $prod_img = collect(
+            $this->ProductsImages->group_and_join('product_id', 'path', 'image_list')
+        )->keyBy('product_id');
+
+        return array_merge_by_int_key($prod, $prod_varat, $prod_cat, $prod_img);
     }
 }
