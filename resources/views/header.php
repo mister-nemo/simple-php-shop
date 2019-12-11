285ceeef4dad27e5f076330884ce3e5e7579bcf2
Mister Nemo <miister.nemo@gmail.com>
2019-12-11 14:18:18 +0200

    Add all links to header

--- resources/views/header.php
+++ resources/views/header.php
@@ -2,9 +2,13 @@
     <div class="inner">
       <h3 class="masthead-brand">Cover</h3>
        <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="/">Home</a>
         <a class="nav-link active" href="/categories">Categories</a>
         <a class="nav-link" href="/products">Products</a>
-        <a class="nav-link" href="/account">Account</a>
+        <a class="nav-link" href="/account">Login</a>
+        <a class="nav-link" href="/register">Register</a>
+        <a class="nav-link" href="/api/categories" target="_blank">API Categories</a>
+        <a class="nav-link" href="/api/products" target="_blank">API Products</a>
       </nav>
     </div>
   </header>
